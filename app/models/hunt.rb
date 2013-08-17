8ea1f31620736609bdd9cf84a216f01cfcd5ac49
emmath <emmatrevena@gmail.com>
2013-08-17 16:03:42 -0700

    added riddles

--- app/models/hunt.rb
+++ app/models/hunt.rb
@@ -2,7 +2,9 @@ class Hunt < ActiveRecord::Base
   attr_accessible :clue, :location, :zipcode
 
   def hint
-    myarray = ['Turn left', 'Why does the caged bird sing?', 'What is the meaning of life?', 'Where are my keys?']
+    myarray = ['Most months have 30 or 31 days. How many months have 28 days?',
+      'What can you catch but not throw?', 'What is the meaning of life?', 'Where are my keys?',
+      'What belongs to you but others use it more than you do?', 'Why did the chicken cross the road?', 'What is full of holes, but can still hold a lot of water?']
     hint_now = myarray[rand(myarray.length)]
     return hint_now
   end
